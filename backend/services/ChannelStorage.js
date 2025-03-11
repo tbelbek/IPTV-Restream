ac78f53cdddfe663c66bc45f1bc42191cbda15ff
antebrl <ahcwnh@gmail.com>
2025-03-11 14:24:38 +0000

    build: set restart policy

--- backend/services/ChannelStorage.js
+++ backend/services/ChannelStorage.js
@@ -8,7 +8,7 @@ const storageFilePath = path.resolve('/channels/channels.json');
 module.exports = {
     load() {
 
-        // Default example channels
+        // Default example channels - OUTDATED!
         const daddyHeaders = [
             { "key": "Origin", "value": "https://cookiewebplay.xyz" },
             { "key": "User-Agent", "value": "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/129.0.0.0 Safari/537.36" },
@@ -22,16 +22,26 @@ module.exports = {
         ];
 
         const defaultChannels = [
-            //Some Test-channels to get started, remove this when using your own playlist
-            //new Channel('Das Erste', "https://mcdn.daserste.de/daserste/de/master.m3u8", "https://upload.wikimedia.org/wikipedia/commons/thumb/5/56/Das_Erste-Logo_klein.svg/768px-Das_Erste-Logo_klein.svg.png", 'direct'),
-            new Channel('DAZN 1 DE', "https://ddy6new.iosplayer.ru/ddy6/premium426/mono.m3u8", "https://upload.wikimedia.org/wikipedia/commons/4/49/DAZN_1.svg", 'proxy', daddyHeaders),
-            new Channel('beIN Sports 1', "https://windnew.koskoros.ru/wind/premium61/mono.m3u8", "https://seeklogo.com/images/B/bein-sports-1-logo-4E5E4AE6B8-seeklogo.com.png", 'proxy', daddyHeaders),
-            new Channel('beIN Sports 2', "https://zekonew.koskoros.ru/zeko/premium90/mono.m3u8", "https://r2.thesportsdb.com/images/media/channel/logo/BeIn_Sports_HD_2_France.png", 'proxy', daddyHeaders),
-            new Channel('Sky Sport Football', "https://zekonew.koskoros.ru/zeko/premium35/mono.m3u8", "https://raw.githubusercontent.com/tv-logo/tv-logos/main/countries/united-kingdom/sky-sports-football-uk.png", 'proxy', daddyHeaders),
-            new Channel('Sky Sports Premier League', "https://dokko1new.koskoros.ru/dokko1/premium130/mono.m3u8", "https://github.com/tv-logo/tv-logos/blob/main/countries/united-kingdom/sky-sports-premier-league-uk.png?raw=true", 'proxy', daddyHeaders),
-            new Channel('SuperSport La Liga', 'https://ddy6new.koskoros.ru/ddy6/premium415/mono.m3u8', "https://www.dstv.com/media/fsmkqhfo/ss_laliga.png?width=330", 'restream', daddyHeaders),
-            //new Channel('TEST', 'https://rr.vipstreams.in/alpha/js/al-taawon-vs-al-qadisiya/1/playlist.m3u8', "https://upload.wikimedia.org/wikipedia/commons/thumb/5/56/Das_Erste-Logo_klein.svg/768px-Das_Erste-Logo_klein.svg.png", 'restream', streamedSuHeaders),
+            //Some Test-channels from daddylive to get started, remove this when using your own playlist
+            new Channel('beIN Sports 1', "https://m3u-playlist-proxy-wine.vercel.app?url=https%3A%2F%2Fdokko1new.koskoros.ru%2Fdokko1%2Fpremium91%2Fmono.m3u8&data=UmVmZXJlcj1odHRwczovL2Rva29wbGF5Lnh5ei98T3JpZ2luPWh0dHBzOi8vZG9rb3BsYXkueHl6", "https://seeklogo.com/images/B/bein-sports-1-logo-4E5E4AE6B8-seeklogo.com.png", 'direct'),
+            new Channel('DAZN 1 DE', "https://m3u-playlist-proxy-wine.vercel.app?url=https%3A%2F%2Fddy6new.koskoros.ru%2Fddy6%2Fpremium426%2Fmono.m3u8&data=UmVmZXJlcj1odHRwczovL2Rva29wbGF5Lnh5ei98T3JpZ2luPWh0dHBzOi8vZG9rb3BsYXkueHl6", "https://upload.wikimedia.org/wikipedia/commons/4/49/DAZN_1.svg", 'direct'),
+            new Channel('DAZN 2 DE', "https://m3u-playlist-proxy-wine.vercel.app?url=https%3A%2F%2Fddy6new.koskoros.ru%2Fddy6%2Fpremium427%2Fmono.m3u8&data=UmVmZXJlcj1odHRwczovL2Rva29wbGF5Lnh5ei98T3JpZ2luPWh0dHBzOi8vZG9rb3BsYXkueHl6", "https://upload.wikimedia.org/wikipedia/commons/thumb/7/76/DAZN_2.svg/2560px-DAZN_2.svg.png", 'direct'),
+            new Channel('Sky Sport Football', "https://m3u-playlist-proxy-wine.vercel.app?url=https%3A%2F%2Fzekonew.koskoros.ru%2Fzeko%2Fpremium35%2Fmono.m3u8&data=UmVmZXJlcj1odHRwczovL2Rva29wbGF5Lnh5ei98T3JpZ2luPWh0dHBzOi8vZG9rb3BsYXkueHl6", "https://raw.githubusercontent.com/tv-logo/tv-logos/main/countries/united-kingdom/sky-sports-football-uk.png", 'direct'),
+            new Channel('Sky Sports Premier League', "https://m3u-playlist-proxy-wine.vercel.app?url=https%3A%2F%2Fdokko1new.koskoros.ru%2Fdokko1%2Fpremium130%2Fmono.m3u8&data=UmVmZXJlcj1odHRwczovL2Rva29wbGF5Lnh5ei98T3JpZ2luPWh0dHBzOi8vZG9rb3BsYXkueHl6", "https://github.com/tv-logo/tv-logos/blob/main/countries/united-kingdom/sky-sports-premier-league-uk.png?raw=true", 'direct'),
+            new Channel('SuperSport La Liga', 'https://m3u-playlist-proxy-wine.vercel.app?url=https%3A%2F%2Fddy6new.koskoros.ru%2Fddy6%2Fpremium415%2Fmono.m3u8&data=UmVmZXJlcj1odHRwczovL2Rva29wbGF5Lnh5ei98T3JpZ2luPWh0dHBzOi8vZG9rb3BsYXkueHl6', "https://www.dstv.com/media/fsmkqhfo/ss_laliga.png?width=330", 'direct'),
         ];
+
+        // const defaultChannels = [
+        //     //Some Test-channels to get started, remove this when using your own playlist
+        //     //new Channel('Das Erste', "https://mcdn.daserste.de/daserste/de/master.m3u8", "https://upload.wikimedia.org/wikipedia/commons/thumb/5/56/Das_Erste-Logo_klein.svg/768px-Das_Erste-Logo_klein.svg.png", 'direct'),
+        //     new Channel('DAZN 1 DE', "https://ddy6new.iosplayer.ru/ddy6/premium426/mono.m3u8", "https://upload.wikimedia.org/wikipedia/commons/4/49/DAZN_1.svg", 'proxy', daddyHeaders),
+        //     new Channel('beIN Sports 1', "https://windnew.koskoros.ru/wind/premium61/mono.m3u8", "https://seeklogo.com/images/B/bein-sports-1-logo-4E5E4AE6B8-seeklogo.com.png", 'proxy', daddyHeaders),
+        //     new Channel('beIN Sports 2', "https://zekonew.koskoros.ru/zeko/premium90/mono.m3u8", "https://r2.thesportsdb.com/images/media/channel/logo/BeIn_Sports_HD_2_France.png", 'proxy', daddyHeaders),
+        //     new Channel('Sky Sport Football', "https://zekonew.koskoros.ru/zeko/premium35/mono.m3u8", "https://raw.githubusercontent.com/tv-logo/tv-logos/main/countries/united-kingdom/sky-sports-football-uk.png", 'proxy', daddyHeaders),
+        //     new Channel('Sky Sports Premier League', "https://dokko1new.koskoros.ru/dokko1/premium130/mono.m3u8", "https://github.com/tv-logo/tv-logos/blob/main/countries/united-kingdom/sky-sports-premier-league-uk.png?raw=true", 'proxy', daddyHeaders),
+        //     new Channel('SuperSport La Liga', 'https://ddy6new.koskoros.ru/ddy6/premium415/mono.m3u8', "https://www.dstv.com/media/fsmkqhfo/ss_laliga.png?width=330", 'restream', daddyHeaders),
+        //     //new Channel('TEST', 'https://rr.vipstreams.in/alpha/js/al-taawon-vs-al-qadisiya/1/playlist.m3u8', "https://upload.wikimedia.org/wikipedia/commons/thumb/5/56/Das_Erste-Logo_klein.svg/768px-Das_Erste-Logo_klein.svg.png", 'restream', streamedSuHeaders),
+        // ];
 
 
         if (fs.existsSync(storageFilePath)) {
